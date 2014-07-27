0f354b2edb353b2d0a6bceecb8360b21b48ff989
Arie Van Antwerp <arievanantwerp@gmail.com>
2014-07-27 00:18:08 -0500

    Fixed ng-click, killed connecting lines

--- js/level-map-controller.js
+++ js/level-map-controller.js
@@ -3,10 +3,13 @@ var app = angular.module('levelApp', ['LocalStorageModule'])
 
     this.defaults = {
       1: [
-        { 'url': 'things.html', 'title': 'Title', 'parents':[], 'solution': 'This is the solution' },
-        { 'url': 'stuff.html', 'title': 'Another Title', 'parents':[], 'solution': 'This is the solution to the second thing' }
+        { 'url': 'thingading.html', 'title': 'The Title', 'parents':[], 'solution': 'This is the wrong solution' },
       ],
       2: [
+        { 'url': 'things.html', 'title': 'Title', 'parents':['thingading.html'], 'solution': 'This is the solution' },
+        { 'url': 'stuff.html', 'title': 'Another Title', 'parents':['thingading.html'], 'solution': 'This is the solution to the second thing' }
+      ],
+      3: [
         { 'url': 'a/things.html', 'title': 'Title', 'parents':['things.html', 'stuff.html'], 'solution': 'This is the solution' },
         { 'url': 'a/stuff.html', 'title': 'Another Title', 'parents':['stuff.html'], 'solution': 'This is the solution to the second thing' }
       ]
@@ -31,39 +34,43 @@ var app = angular.module('levelApp', ['LocalStorageModule'])
       // })
     }
 
-
-
-
-
     this.init()
 
   }])
-  .directive('level', function($timeout) {
+  .directive('levelinfo', function($timeout) {
     return {
       restrict: 'E',
       scope: {
         level: '='
       },
-      templateUrl: 'templates/level.html',
+      //templateUrl: 'templates/level.html',
+      template: '<section id="{{level.url}}"><p>{{level.url}}</p><p>{{level.title}}</p><article><div class="revealSolution" ng-click="connectLevels()" onclick="console.log(\'things\')"></div>{{level.solution}}</article></section>',
       link: function(scope, element, attrs) {
 
         scope.connect = connect
 
+        scope.do = function() {
+          console.log("THINGS!")
+        //   //console.log(element.find('article'))
+        }
+
         var connectLevels = function(){
           scope.level.parents.forEach(function(parent){
-            console.log(scope.level.url)
-            scope.connect(document.getElementById(scope.level.url), document.getElementById(parent), 'things', 'stuff')
+            //document.getElementById(scope.level.url)
+            scope.connect(element, document.getElementById(scope.level.url), document.getElementById(parent), 'things')
           })
         }
 
-        $timeout(connectLevels, 0)
+        scope.connectLevels = connectLevels
+
+        $timeout.cancel($timeout(connectLevels, 0))
 
       }
     }
   })
 
     //http://stackoverflow.com/questions/8672369/how-to-draw-a-line-between-two-divs
-    var connect = function(div1, div2, color, thickness) {
+    var connect = function(elm, div1, div2, color) {
       var off1 = getOffset(div1)
       var off2 = getOffset(div2)
       // top center
@@ -76,13 +83,14 @@ var app = angular.module('levelApp', ['LocalStorageModule'])
       var length = Math.sqrt(((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1)))
       // center
       var cx = ((x1 + x2) / 2) - (length / 2)
-      var cy = ((y1 + y2) / 2) - (2 / 2)
+      var cy = ((y1 + y2) / 2) - 1
       // angle
       var angle = Math.atan2((y1-y2),(x1-x2))*(180/Math.PI)
-      // make hr
+      // // make hr
       var htmlLine = "<div style='padding:0px; margin:0px; height:2px; background-color:rgba(0,0,0,.6); line-height:1px; position:absolute; left:" + cx + "px; top:" + cy + "px; width:" + length + "px; -moz-transform:rotate(" + angle + "deg); -webkit-transform:rotate(" + angle + "deg); -o-transform:rotate(" + angle + "deg); -ms-transform:rotate(" + angle + "deg); transform:rotate(" + angle + "deg);' />"
 
-      document.body.innerHTML += htmlLine
+      //document.body.innerHTML += htmlLine
+      elm.append(htmlLine)
     }
 
     var getOffset = function(el) {
@@ -90,6 +98,8 @@ var app = angular.module('levelApp', ['LocalStorageModule'])
         var y = 0
         var w = el.offsetWidth || 0
         var h = el.offsetHeight || 0
+        // //needs debugging for scroll
+        // //infine loop. :(
         while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
             x += el.offsetLeft - el.scrollLeft
             y += el.offsetTop - el.scrollTop
